e1e32518820aacb23d96d60889d83636eee45703
pixel6384 <helix7342@protonmail.com>
2026-09-24 14:35:26 +0700

    Implement average aggregation for filtered metrics

--- src/Main.hs
+++ src/Main.hs
@@ -6,11 +6,23 @@ import Conduit
 import qualified Data.Conduit.Binary as CB
 import qualified Data.Conduit.Text as CT
 import Sieve
+import Control.Monad.IO.Class (liftIO)
 
 main :: IO ()
 main = do
   args <- getArgs
   case args of
+    ["avg", target, thresholdStr] -> do
+      let threshold = read thresholdStr :: Double
+      metrics <- runConduitRes $ 
+        CB.sourceHandle stdin
+        .| CT.decodeUtf8
+        .| CT.lines
+        .| mapC parseLine
+        .| concatMapC id
+        .| filterC (filterMetric (T.pack target) threshold)
+        .| sinkList
+      putStrLn $ "Average: " ++ show (aggregateMetrics metrics)
     [target, thresholdStr] -> do
       let threshold = read thresholdStr :: Double
       runConduitRes $ 
@@ -21,4 +33,6 @@ main = do
         .| concatMapC id
         .| filterC (filterMetric (T.pack target) threshold)
         .| mapM_C (liftIO . print)
-    _ -> putStrLn "Usage: metric-sieve <metric_name> <threshold>"
+    _ -> putStrLn "Usage:
+  metric-sieve <metric_name> <threshold>
+  metric-sieve avg <metric_name> <threshold"
